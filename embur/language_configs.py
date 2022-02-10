6f7e0f9b6d0f07790575f1de9796f8c83108111e
Luke Gessler <lukegessler@gmail.com>
2022-02-09 23:33:54 -0500

    add lang configs for all but latin
    also eval on dev by default

--- embur/language_configs.py
+++ embur/language_configs.py
@@ -6,7 +6,49 @@ from all dicts). Be smart in this file and do as much as possible to only write
 data, not code!
 """
 
-LANGUAGES = ["coptic", "maltese"]
+LANGUAGES = ["coptic", "maltese", "wolof", "uyghur", "greek", "latin"]
+
+def _std_pretrain_config(mismatched_reader, language, treebank_name, conllu_name, ssplit_type="_punct"):
+    return {
+        "train_data_paths": {
+            "xpos": f"data/{language}/{treebank_name}/{conllu_name}-train.conllu",
+            "mlm": f"data/{language}/converted{ssplit_type}/train",
+            "parser": f"data/{language}/{treebank_name}/{conllu_name}-train.conllu"
+        },
+        "dev_data_paths": {
+            "xpos": f"data/{language}/{treebank_name}/{conllu_name}-dev.conllu",
+            "mlm": f"data/{language}/converted{ssplit_type}/dev",
+            "parser": f"data/{language}/{treebank_name}/{conllu_name}-dev.conllu"
+        },
+        "readers": {
+            "xpos": mismatched_reader,
+            "mlm": mismatched_reader,
+            "parser": mismatched_reader
+        },
+        "tokenizer_conllu_path": f"data/{language}/converted{ssplit_type}/train"
+    }
+
+
+def _std_eval_config(model_name, language, treebank_name, conllu_name):
+    return {
+        "training": {
+            "dataset_reader": {
+                "type": "embur_conllu",
+                "token_indexers": {
+                    "tokens": {
+                        "type": "pretrained_transformer_mismatched",
+                        "model_name": model_name
+                    }
+                }
+            },
+            "train_data_path": f"data/{language}/{treebank_name}/{conllu_name}-train.conllu",
+            "validation_data_path": f"data/{language}/{treebank_name}/{conllu_name}-dev.conllu"
+        },
+        "testing": {
+            "input_file": f"data/{language}/{treebank_name}/{conllu_name}-dev.conllu"
+        }
+    }
+
 
 def get_pretrain_config(language, tokenizer_path, excluded_tasks):
     """
@@ -43,24 +85,10 @@ def get_pretrain_config(language, tokenizer_path, excluded_tasks):
             },
             "tokenizer_conllu_path": "data/coptic/converted/train"
         },
-        "maltese": {
-            "train_data_paths": {
-                "xpos": "data/maltese/UD_Maltese-MUDT/mt_mudt-ud-train.conllu",
-                "mlm": "data/maltese/converted_punct/train",
-                "parser": "data/maltese/UD_Maltese-MUDT/mt_mudt-ud-train.conllu"
-            },
-            "dev_data_paths": {
-                "xpos": "data/maltese/UD_Maltese-MUDT/mt_mudt-ud-dev.conllu",
-                "mlm": "data/maltese/converted_punct/dev",
-                "parser": "data/maltese/UD_Maltese-MUDT/mt_mudt-ud-dev.conllu"
-            },
-            "readers": {
-                "xpos": mismatched_reader,
-                "mlm": mismatched_reader,
-                "parser": mismatched_reader
-            },
-            "tokenizer_conllu_path": "data/maltese/converted_punct/train"
-        }
+        "maltese": _std_pretrain_config(mismatched_reader, "maltese", "UD_Maltese-MUDT", "mt_mudt-ud", ssplit_type="punct"),
+        "wolof": _std_pretrain_config(mismatched_reader, "wolof", "UD_Wolof-WTB", "wo_wtb-ud", ssplit_type="punct"),
+        "uyghur": _std_pretrain_config(mismatched_reader, "uyghur", "UD_Uyghur-UDT", "ug_udt-ud", ssplit_type="punct"),
+        "greek": _std_pretrain_config(mismatched_reader, "greek", "UD_Ancient_Greek-PROIEL", "grc_proiel-ud", ssplit_type="punct")
     }[language]
 
     for subconfig_name, subconfig in language_config.items():
@@ -95,25 +123,11 @@ def get_eval_config(language, model_name):
                 "validation_data_path": "data/coptic/UD_Coptic-Scriptorium/cop_scriptorium-ud-dev.conllu"
             },
             "testing": {
-                "input_file": "data/coptic/UD_Coptic-Scriptorium/cop_scriptorium-ud-test.conllu"
+                "input_file": "data/coptic/UD_Coptic-Scriptorium/cop_scriptorium-ud-dev.conllu"
             }
         },
-        "maltese": {
-            "training": {
-                "dataset_reader": {
-                    "type": "embur_conllu",
-                    "token_indexers": {
-                        "tokens": {
-                            "type": "pretrained_transformer_mismatched",
-                            "model_name": model_name
-                        }
-                    }
-                },
-                "train_data_path": "data/maltese/UD_Maltese-MUDT/mt_mudt-ud-train.conllu",
-                "validation_data_path": "data/maltese/UD_Maltese-MUDT/mt_mudt-ud-dev.conllu"
-            },
-            "testing": {
-                "input_file": "data/maltese/UD_Maltese-MUDT/mt_mudt-ud-test.conllu"
-            }
-        }
+        "maltese": _std_eval_config(model_name, "maltese", "UD_Maltese-MUDT", "mt_mudt-ud"),
+        "wolof": _std_eval_config(model_name, "wolof", "UD_Wolof-WTB", "wo_wtb-ud"),
+        "uyghur": _std_pretrain_config(model_name, "uyghur", "UD_Uyghur-UDT", "ug_udt-ud"),
+        "greek": _std_pretrain_config(model_name, "greek", "UD_Ancient_Greek-PROIEL", "grc_proiel-ud")
     }[language]
