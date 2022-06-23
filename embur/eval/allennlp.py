d8bd98a5fc2b2b5f5f7e6ab65ccf68de9fb5afd2
Luke Gessler <lukegessler@gmail.com>
2022-06-23 01:24:33 -0400

    oops

---
+++ embur/eval/allennlp.py
@@ -0,0 +1,64 @@
+import argparse
+import json
+import os
+from logging import getLogger
+from torch.serialization import mkdtemp
+
+from allennlp.commands.evaluate import evaluate_from_args
+from allennlp.commands.train import train_model_from_file
+from transformers import BertModel
+
+logger = getLogger(__name__)
+
+
+def eval_args(serialization_dir, input_file):
+    args = argparse.Namespace()
+    args.file_friendly_logging = False
+    args.weights_file = None
+    args.overrides = None
+    args.embedding_sources_mapping = None
+    args.extend_vocab = False
+    args.batch_weight_key = None
+    args.batch_size = None
+    args.archive_file = f"{serialization_dir}/model.tar.gz"
+    args.input_file = input_file
+    args.output_file = serialization_dir + "/metrics"
+    args.predictions_output_file = serialization_dir + "/predictions"
+    args.cuda_device = 0
+    args.auto_names = "NONE"
+    return args
+
+
+def evaluate_allennlp(config):
+    """
+    Given a language and a pretrained BERT model (or something API compatible with it),
+    """
+    with mkdtemp() as eval_dir:
+        logger.info("Training for evaluation")
+        bert_model = BertModel.from_pretrained(config.bert_model_name)
+        language_config = config.parser_eval_language_config
+
+        os.environ["BERT_DIMS"] = str(bert_model.config.hidden_size)
+        os.environ["BERT_PATH"] = config.bert_model_name
+        os.environ["TRAINABLE"] = "0"
+        for k, v in language_config["training"].items():
+            os.environ[k] = json.dumps(v) if isinstance(v, dict) else v
+
+        overrides = ""
+        if config.finetune or config.debug:
+            overrides += "{"
+            if config.debug:
+                overrides += '"trainer.num_epochs": 1'
+                if config.finetune:
+                    overrides += ","
+            if config.finetune:
+                overrides += '"model.text_field_embedder.token_embedders.tokens.train_parameters": true'
+            overrides += "}"
+
+        train_model_from_file(config.parser_eval_jsonnet, eval_dir, overrides=overrides)
+
+        logger.info("Evaluating")
+        args = eval_args(eval_dir, language_config["testing"]["input_file"])
+        metrics = evaluate_from_args(args)
+    logger.info(metrics)
+    return None, metrics
