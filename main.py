7a8497cdb75912efdb3d63aadd6fb45f2b3486f4
Luke Gessler <lg876@georgetown.edu>
2022-06-26 19:24:15 -0400

    make pretrain_evaluate log

--- main.py
+++ main.py
@@ -46,12 +46,26 @@ import_module_and_submodules("allennlp_models")
     type=click.Choice(TASKS),
     help="Specify task(s) to exclude from a run. Possible values: mlm, parser, xpos",
 )
-@click.option("--tokenization-type", type=click.Choice(TOKENIZATION_TYPES), default="wordpiece")
-@click.option("--num-layers", default=3, type=int, help="Number of BERT encoder block layers")
-@click.option("--num-attention-heads", default=5, type=int, help="Number of BERT attention heads")
+@click.option(
+    "--tokenization-type", type=click.Choice(TOKENIZATION_TYPES), default="wordpiece"
+)
+@click.option(
+    "--num-layers", default=3, type=int, help="Number of BERT encoder block layers"
+)
+@click.option(
+    "--num-attention-heads", default=5, type=int, help="Number of BERT attention heads"
+)
 @click.option("--embedding-dim", default=100, type=int, help="BERT hidden dimension")
-@click.option("--finetune/--no-finetune", default=False, help="Whether to make BERT params trainable during evaluation")
-@click.option("--debug/--no-debug", default=False, help="When set, use toy data and small iteration counts to debug")
+@click.option(
+    "--finetune/--no-finetune",
+    default=False,
+    help="Whether to make BERT params trainable during evaluation",
+)
+@click.option(
+    "--debug/--no-debug",
+    default=False,
+    help="When set, use toy data and small iteration counts to debug",
+)
 @click.pass_context
 def top(ctx, **kwargs):
     ctx.obj = Config(**kwargs)
@@ -67,9 +81,21 @@ def word2vec_evaluate(config):
     config.finetune = True  # always finetune
     config.parser_eval_jsonnet = "configs/static_parser_eval.jsonnet"
 
-    documents = read_conllu_files(config.pretrain_language_config["tokenizer_conllu_path"])
-    sentences = [[t["form"] for t in sentence] for document in documents for sentence in document]
-    model = Word2Vec(sentences=sentences, sg=1, negative=5, vector_size=100, window=5, min_count=1, workers=4)
+    documents = read_conllu_files(
+        config.pretrain_language_config["tokenizer_conllu_path"]
+    )
+    sentences = [
+        [t["form"] for t in sentence] for document in documents for sentence in document
+    ]
+    model = Word2Vec(
+        sentences=sentences,
+        sg=1,
+        negative=5,
+        vector_size=100,
+        window=5,
+        min_count=1,
+        workers=4,
+    )
 
     os.makedirs("word2vec", exist_ok=True)
     with open(config.word2vec_file, "w") as f:
@@ -92,7 +118,9 @@ def word2vec_evaluate(config):
     _locked_write("metrics.tsv", output + "\n")
 
 
-@click.command(help="Run the pretraining phase of an experiment where a BERT model is trained")
+@click.command(
+    help="Run the pretraining phase of an experiment where a BERT model is trained"
+)
 @click.pass_obj
 def pretrain_evaluate(config):
     # Prepare directories that will be used for the AllenNLP model and the extracted BERT model
@@ -100,9 +128,17 @@ def pretrain_evaluate(config):
 
     # Get config and train tokenizer
     print("Training tokenizer...")
-    documents = read_conllu_files(config.pretrain_language_config["tokenizer_conllu_path"])
-    sentences = [" ".join([t["form"] for t in sentence]) for document in documents for sentence in document]
-    train_tokenizer(sentences, serialize_path=config.bert_dir, model_type=config.tokenization_type)
+    documents = read_conllu_files(
+        config.pretrain_language_config["tokenizer_conllu_path"]
+    )
+    sentences = [
+        " ".join([t["form"] for t in sentence])
+        for document in documents
+        for sentence in document
+    ]
+    train_tokenizer(
+        sentences, serialize_path=config.bert_dir, model_type=config.tokenization_type
+    )
 
     # these are needed by bert_pretrain.jsonnet
     config.prepare_bert_pretrain_env_vars()
@@ -111,15 +147,31 @@ def pretrain_evaluate(config):
     print("Beginning pretraining...")
     print("Config:\n", config.pretrain_language_config)
     print("Env:\n", os.environ)
-    overrides = '{"trainer.num_epochs": 1, "data_loader.instances_per_epoch": 256}' if config.debug else ""
-    model = train_model_from_file(config.pretrain_jsonnet, config.experiment_dir, overrides=overrides)
+    overrides = (
+        '{"trainer.num_epochs": 1, "data_loader.instances_per_epoch": 256}'
+        if config.debug
+        else ""
+    )
+    model = train_model_from_file(
+        config.pretrain_jsonnet, config.experiment_dir, overrides=overrides
+    )
     bert_serialization: BertModel = model._backbone.bert
     bert_serialization.save_pretrained(config.bert_dir)
     with open(os.path.join(config.experiment_dir, "metrics.json"), "r") as f:
         train_metrics = json.load(f)
 
     eval_metrics = evaluate_allennlp(config)
-    return train_metrics, eval_metrics
+    _, metrics = eval_metrics
+    output = "\t".join(
+        [
+            config.language,
+            "_".join(config.tasks) + ("_ft" if config.finetune else ""),
+            "_",
+            "_",
+            str(metrics["LAS"]),
+        ]
+    )
+    _locked_write("metrics.tsv", output + "\n")
 
 
 def _locked_write(filepath, s):
@@ -169,35 +221,11 @@ def language_trial(ctx):
 
     # MLM only
     ctx.obj.set_tasks(["mlm"])
-    mlm_only_metrics_train, mlm_only_metrics_eval = ctx.invoke(pretrain_evaluate)
-    _, metrics = mlm_only_metrics_eval
-    output = "\t".join(
-        [
-            config.language,
-            "mlm_only",
-            "",
-            "",
-            str(metrics["LAS"]),
-        ]
-    )
-    _locked_write("metrics.tsv", output + "\n")
+    ctx.invoke(pretrain_evaluate)
 
     # All MTL tasks
     ctx.obj.set_tasks(["mlm", "xpos", "parser"])
-    mtl_metrics_train, mtl_metrics_eval = ctx.invoke(
-        pretrain_evaluate,
-    )
-    _, metrics = mtl_metrics_eval
-    output = "\t".join(
-        [
-            config.language,
-            "mtl",
-            str(mtl_metrics_train["training_parser_LAS"]),
-            "_",
-            str(metrics["LAS"]),
-        ]
-    )
-    _locked_write("metrics.tsv", output + "\n")
+    ctx.invoke(pretrain_evaluate)
 
 
 top.add_command(pretrain_evaluate)
